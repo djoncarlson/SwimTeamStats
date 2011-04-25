d1c355db47138b4115db6011e1cff66206e975aa
Devin <djoncarlson@gmail.com>
2011-04-24 19:58:19 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Swimteamstats::Application.config.secret_token = 'e88f6d5353b1c4b09aa4d09e2a4e7916cf1a5eb54661471064aa3ef480998e49f5853c038e42e362db6f1759749cee54ebab749434e7adae8830d4b54bebe50b'
